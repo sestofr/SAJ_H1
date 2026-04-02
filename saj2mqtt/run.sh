205aa3681ac92be51b393ad7f331ff01d6ae5b68
sestofr <157533789+sestofr@users.noreply.github.com>
2026-04-02 23:16:45 +0200

    Update run.sh

--- saj2mqtt/run.sh
+++ saj2mqtt/run.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+
+export SAJ_HOST=$(bashio::config 'saj_host')
+export SAJ_PORT=$(bashio::config 'saj_port')
+export MQTT_HOST=$(bashio::config 'mqtt_host')
+export MQTT_PORT=$(bashio::config 'mqtt_port')
+export MQTT_USERNAME=$(bashio::config 'mqtt_user')
+export MQTT_PASSWORD=$(bashio::config 'mqtt_pass')
+
+echo "Starting SAJ2MQTT..."
+/usr/bin/saj2mqtt
 
